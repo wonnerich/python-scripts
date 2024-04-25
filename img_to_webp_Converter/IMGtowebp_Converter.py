969b818cace92811c8a8e501eb29aaf8574e50f0
wonnerich <fabian.looman@gmail.com>
2024-04-26 00:48:06 +0200

    all files in directory

--- img_to_webp_Converter/IMGtowebp_Converter.py
+++ img_to_webp_Converter/IMGtowebp_Converter.py
@@ -1,6 +1,13 @@
+import os
 from PIL import Image
 
-im = Image.open("WonneMedia_ogImage.png").convert("RGB")
+def convert_images_to_webp(directory, quality=80):
+    for filename in os.listdir(directory):
+        if filename.endswith(('.png', '.jpg', '.jpeg')):
+            img = Image.open(os.path.join(directory, filename)).convert("RGB")
+            webp_filename = os.path.splitext(filename)[0] + '.webp'
+            img.save(os.path.join(directory, webp_filename), 'WEBP', quality=quality)
 
-im.save("output.webp", "WEBP", quality=80)
+# Usage
+convert_images_to_webp('/path/to/your/directory')
 
